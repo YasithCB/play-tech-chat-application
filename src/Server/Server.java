c79e2a38580314b69f94e60fda1d38f4d4b328a8
YasithCB <yasith.cb2001@gmail.com>
2022-08-16 10:50:59 +0530

    Server and ClientHandler classes added

---
+++ src/Server/Server.java
@@ -0,0 +1,33 @@
+package Server;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.ArrayList;
+
+/**
+ * author  Yasith C Bandara
+ * created 8/16/2022 - 10:45 AM
+ * project Playtech
+ */
+
+public class Server {
+    private static ArrayList<ClientHandler> clients = new ArrayList<ClientHandler>();
+
+    public static void main(String[] args) throws IOException {
+
+        ServerSocket serverSocket=new ServerSocket(5001);
+
+        Socket socket;
+
+        while (true){
+
+            System.out.println("Waiting for Client ...");
+            socket= serverSocket.accept();
+            System.out.println("Client Connected");
+            ClientHandler clientThread = new ClientHandler(socket, clients);
+            clients.add(clientThread);
+            clientThread.start();
+        }
+    }
+}
