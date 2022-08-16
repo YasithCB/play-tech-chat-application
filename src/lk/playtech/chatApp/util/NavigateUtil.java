3f5e43b3b1022ada192171d87023342bbe9136ee
YasithCB <yasith.cb2001@gmail.com>
2022-08-16 11:12:03 +0530

    Util classes created

---
+++ src/lk/playtech/chatApp/util/NavigateUtil.java
@@ -0,0 +1,36 @@
+package lk.playtech.chatApp.util;
+
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+/**
+ * author  Yasith C Bandara
+ * created 8/15/2022 - 9:38 PM
+ * project Playtech
+ */
+
+public class NavigateUtil {
+    public static void setUi(AnchorPane apn, String location, String title) throws IOException {
+        Stage stage = (Stage) apn.getScene().getWindow();
+        Parent root = FXMLLoader.load(NavigateUtil.class.getResource("../view/" + location + ".fxml"));
+        stage.setScene(new Scene(root));
+
+        /*root.setOnMousePressed(event -> {
+            x = event.getSceneX();
+            y = event.getSceneY();
+        });
+        root.setOnMouseDragged(event -> {
+            stage.setX(event.getScreenX() - x);
+            stage.setY(event.getScreenY() - y);
+        });*/
+
+        stage.setTitle(title);
+        stage.centerOnScreen();
+        stage.show();
+    }
+}
